88a58656bb3de8c454905b81b7ca71e259328f2b
Himanshu Gupta <thinktank.himanshu@gmail.com>
2023-04-17 20:29:07 +0530

    api bug fix

--- pages/api/whisper.js
+++ pages/api/whisper.js
@@ -10,9 +10,6 @@ export const config = {
 
 export default withFileUpload(async (req, res) => {
   const file = req.file;
-  const language = req.fields.language;
-
-  console.log('language: ', language);
 
   if (!file) {
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -22,29 +19,20 @@ export default withFileUpload(async (req, res) => {
   const formData = new FormData();
   formData.append('file', createReadStream(file.filepath), 'audio.wav');
   formData.append('model', 'whisper-1');
-  formData.append(
-    'prompt',
-    'The transcript is about AVA: Adaptive Voice Assistant'
-  );
 
-  if (language !== 'auto') formData.append('language', language);
-
-  const response = await fetch(
-    'https://api.openai.com/v1/audio/transcriptions',
-    {
-      method: 'POST',
-      headers: {
-        ...formData.getHeaders(),
-        Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
-      },
-      body: formData,
-    }
-  );
+  const response = await fetch('https://api.openai.com/v1/audio/translations', {
+    method: 'POST',
+    headers: {
+      ...formData.getHeaders(),
+      Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+    },
+    body: formData,
+  });
 
   const { text, error } = await response.json();
   if (response.ok) {
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.status(200).json({ text: text, lang: language });
+    res.status(200).json({ text: text });
   } else {
     console.log('OPEN AI ERROR:');
     console.log(error.message);
